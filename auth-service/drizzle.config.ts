6f14c20bfa098ae4e233b01c7f0d0022fda3a972
Vlad Kalashnikov <spotless.quash@gmail.com>
2026-03-07 17:05:29 +0000

    fix(auth-service): correct drizzle config after refactor

--- auth-service/drizzle.config.ts
+++ auth-service/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./modules/db/schema.ts",
-  out: "./modules/db/drizzle",
+  schema: "./modules/db-module/schema.ts",
+  out: "./modules/db-module/drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
